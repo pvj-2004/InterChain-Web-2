f4b25965ec39979a0638cfc4efcf3cc32dfee339
pvj-2004 <prathameshjanugade2004@gmail.com>
2025-01-04 17:57:34 +0530

    Add files via upload

---
+++ src/assets/data/artworkDetails.jsx
@@ -0,0 +1,54 @@
+import modernLifeImage from "../images/Modern_Life.png";
+import riceFieldsImage from "../images/Rice_Fields.png";
+import abstractPaintingImage from "../images/Abstract-Painting.png";
+import wavesImage from "../images/Waves.png";
+import galaxyImage from "../images/Galaxy.png";
+import Brain from "../images/Brain.jpg"
+import Lotus from "../images/Lotus.jpg"
+import Technology from "../images/Technology.png"
+
+
+const artworkDetails = [
+  {
+    Name: `"Mordern Life Image"`,
+    Description: "~Painted by the great Boy of the Boys~",
+    imageSource: modernLifeImage,
+  },
+  {
+    Name: `"Rice Fields"`,
+    Description: "~Painted by the great Girl of the Girls~",
+    imageSource: riceFieldsImage,
+  },
+  {
+    Name: `"Abstract Painting"`,
+    Description: "~Painted by the great Man of the Men~",
+    imageSource: abstractPaintingImage,
+  },
+  {
+    Name: `"Vibrant Waves"`,
+    Description: "~Painted by the great Woman of the Women~",
+    imageSource: wavesImage,
+  },
+  {
+    Name: `"Galaxy"`,
+    Description: "~Painted by the great God of the Gods~",
+    imageSource: galaxyImage,
+  },
+  {
+    Name: `"Brain"`,
+    Description: "~Painted by the great God of the Gods~",
+    imageSource: Brain,
+  },
+  {
+    Name: `"Lotus"`,
+    Description: "~Painted by the great God of the Gods~",
+    imageSource: Lotus,
+  },
+  {
+    Name: `"Technological Art"`,
+    Description: "~Painted by the great God of the Gods~",
+    imageSource: Technology,
+  },
+];
+
+export { artworkDetails };
